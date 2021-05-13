2b140669c3b1ccb52097475d98102d8e895e0ad7
Lakshay Sood <lksood@ymail.com>
2021-05-13 21:44:00 +0530

    bug fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -28,9 +28,7 @@ exports.createCheckoutSession = catchAsync(async (req, res, next) => {
         name: tour.name,
         description: tour.summary,
         images: [
-          `${req.protocol}://${req.get('host')}/public/img/tours/${
-            tour.imageCover
-          }`
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
         ],
         amount: tour.price * 100,
         currency: 'usd',
@@ -60,6 +58,8 @@ exports.createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
+
+  await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
